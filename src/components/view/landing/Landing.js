2c352eec5dc31d07c9a60c63cd11a7c0436b04dc
Ashraful Islam <ashrafulrobin3@gmail.com>
2019-10-07 00:15:09 +0600

    * Added react-markdown for markdown showing * Added basic api call for test markdown with real data

--- src/components/view/landing/Landing.js
+++ src/components/view/landing/Landing.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
 
 export default class LandingPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            article: {}
+        }
+    }
+    getAnArticle(id=89784) {
+        fetch('https://dev.to/api/articles/' + id).then((response) => {
+            return response.json()
+        }).then((article) => {
+            this.setState({article: article})
+        })
+    }
+
     render() {
+        this.getAnArticle();
         return (
-            <div>
+            <div className="container">
                 <h2>Landing page</h2>
                 <hr/>
+                <ReactMarkdown source={
+                    this.state.article.body_markdown
+                }/>
             </div>
         )
     }
