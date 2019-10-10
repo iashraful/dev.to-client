b393285bd3f1da57c8a3148eb7f790eb9b5709ed
Ashraful Islam <ashrafulrobin3@gmail.com>
2019-10-10 20:34:33 +0600

    - Removed react files - Added vuejs app

--- src/utils/article-utils.js
+++
@@ -1,7 +0,0 @@
-export function devPostMarkdownParser(body) {
-    let parsedBody = body.split('---');
-    parsedBody = parsedBody.length > 1 ? parsedBody[2] : parsedBody[0];
-    // parsedBody = parsedBody.replace('\n', '  ');
-    // console.log(parsedBody)
-    return parsedBody
-}
