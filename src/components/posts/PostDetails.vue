83fe4b64199fe1882e09432c0a5a945264868a5a
Ashraful Islam <ashrafulrobin3@gmail.com>
2019-10-12 17:33:23 +0600

    Updated post body design

--- src/components/posts/PostDetails.vue
+++ src/components/posts/PostDetails.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
+    <div class="post-details">
         <p class="title has-text-centered is-4">{{ post.title }}</p>
+        <ui-tag :tags="post.tags"/>
         <figure class="image is-4by2">
             <img :src="post.cover_image">
         </figure>
@@ -13,11 +14,13 @@
 <script>
     import VueMarkdown from 'vue-markdown'
     import postUtils from '@/utils/posts'
+    import UiTag from "@/components/common/UiTag";
 
     export default {
         name: "PostDetails",
         props: ['post'],
         components: {
+            UiTag,
             VueMarkdown
         },
         methods: {
