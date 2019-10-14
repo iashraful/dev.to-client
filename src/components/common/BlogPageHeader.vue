763ed92eecf51ad76249084cb279c274b53eedab
Ashraful Islam <ashrafulrobin3@gmail.com>
2019-10-14 23:49:44 +0600

    added code font family class

--- src/components/common/BlogPageHeader.vue
+++ src/components/common/BlogPageHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="page-header">
-        <h1 class="title has-text-centered">{{ title }}</h1>
+        <h1 class="title has-text-centered is-family-code">{{ title }}</h1>
         <h1 class="subtitle has-text-centered">{{ subtitle }}</h1>
     </div>
 </template>
